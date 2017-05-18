62b217f8964ac18a2c5e6e7aec9a73503bdea3c8
costi31 <lucaluciano.costanzo@mail.polimi.it>
2017-05-19 01:21:33 +0200

    Add findById, remove and changed create.
    I have added the GET request to get user info by its id.
    Added DELETE request to remove a user (only if it's authenticated as the desired user).
    Changed create method fields

--- src/main/java/com/herokuapp/polimiboardgamemanager/resources/UserResource.java
+++ src/main/java/com/herokuapp/polimiboardgamemanager/resources/UserResource.java
@@ -2,28 +2,27 @@ package com.herokuapp.polimiboardgamemanager.resources;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 
 import com.herokuapp.polimiboardgamemanager.dao.UserDao;
+import com.herokuapp.polimiboardgamemanager.filter.Secured;
 import com.herokuapp.polimiboardgamemanager.model.User;
-
-import javax.xml.bind.annotation.XmlAccessType;
 
 @Path("/users")
 @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_XML})
@@ -51,13 +50,13 @@ public class UserResource {
         try {
 
             // Authenticate the user using the credentials provided
-            UserDao.getInstance().authenticate(username, password);
+            long userId = UserDao.getInstance().authenticate(username, password);
 
             // Issue a token for the user
-            String token = UserDao.getInstance().issueToken(username, uriInfo);
+            String token = UserDao.getInstance().issueToken(userId, username, uriInfo);
 
             // Return the token on the response
-            return Response.ok(token).build();
+            return Response.ok().header(HttpHeaders.AUTHORIZATION,  "Bearer " + token).build();
 
         } catch (Exception e) {
             return Response.status(Response.Status.UNAUTHORIZED).build();
@@ -65,18 +64,61 @@ public class UserResource {
     }
     
     @POST
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON} )
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_XML})
-    public Response create(User user) {
-        UserDao.getInstance().createUser(user);
-        return Response.created(uriInfo.getAbsolutePathBuilder().path(String.valueOf(user.getId())).build()).build();
-    }
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response create(@FormParam("fullName") String fullName,
+                           @FormParam("username") String username, 
+                           @FormParam("password") String password
+                           ) {
+        try {
+            long id = UserDao.getInstance().createUser(fullName, username, password);
+            return Response.created(uriInfo.getAbsolutePathBuilder().path(String.valueOf(id)).build()).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.CONFLICT).entity(e.getMessage()).build();
+        }
+        
+    }    
+    
+//    @POST
+//    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON} )
+//    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_XML})
+//    public Response create(User user) {
+//        try {
+//            UserDao.getInstance().createUser(user);
+//            return Response.created(uriInfo.getAbsolutePathBuilder().path(String.valueOf(user.getId())).build()).build();
+//        } catch (Exception e) {
+//            return Response.status(Response.Status.CONFLICT).entity(e.getMessage()).build();
+//        }
+//        
+//    }
     
     @GET
     public Response findAllUsers() {
         GenericEntity<List<User>> list = new GenericEntity<List<User>>(UserDao.getInstance().findAllUsersNameOrd()){};
         return Response.ok(list).build();
     }
+    
+    @GET
+    @Path("/{id}")
+    public Response findById(@PathParam("id") Long id) {
+        User user = UserDao.getInstance().findById(id);
+
+        if (user == null)
+            return Response.status(Response.Status.NOT_FOUND).build();
+
+        return Response.ok(user).build();
+    }    
+    
+    @DELETE
+    @Path("/{id}")
+    @Secured
+    public Response remove(@PathParam("id") Long id, @HeaderParam(HttpHeaders.AUTHORIZATION) String authorizationBearer) {
+        try {
+            UserDao.getInstance().removeUser(id, authorizationBearer);
+            return Response.noContent().build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+    }    
     
 //    @POST
 //    @Produces(MediaType.APPLICATION_JSON)
