e89313866a465eacd2264e6b40317bfc9378e95e
costi31 <lucaluciano.costanzo@mail.polimi.it>
2017-06-18 20:53:53 +0200

    Fix error with response

--- src/main/java/com/herokuapp/polimiboardgamemanager/resources/RootResource.java
+++ src/main/java/com/herokuapp/polimiboardgamemanager/resources/RootResource.java
@@ -12,8 +12,14 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import com.herokuapp.polimiboardgamemanager.model.CommonConstants;
+import com.herokuapp.polimiboardgamemanager.model.LinkAdapter;
 
 /**
  * Root resource that responds only to http GET requests to "/" and
@@ -50,12 +56,41 @@ public class RootResource {
      */
     @GET
     public Response getLinks() {
-        List<Link> links = new ArrayList<>();
-        links.add(Link.fromUri(CommonConstants.API_URI+"users/login").rel("login").build());
-        links.add(Link.fromUri(CommonConstants.API_URI+"users/").rel("users").build());
-        links.add(Link.fromUri(CommonConstants.API_URI+"boardgames/").rel("boardgames").build());
-
-        return Response.ok(links.toArray(new Link[links.size()])).links(links.toArray(new Link[links.size()])).build();
+    	Root res = new Root();
+    	
+    	List<Link> links = res.getLinks();
+    	
+        return Response.ok(res).links(links.toArray(new Link[links.size()])).build();
+    }
+    
+    @XmlRootElement
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public class Root {        
+        /**
+         * Gets the links list.
+         *
+         * @return the links list
+         */
+        @XmlElement(name = "link")
+        @XmlJavaTypeAdapter(LinkAdapter.class)
+        public List<Link> getLinks() {
+            List<Link> links = new ArrayList<>();
+            links.add(Link.fromUri(CommonConstants.API_URI+"users/login").rel("login").build());
+            links.add(Link.fromUri(CommonConstants.API_URI+"users/").rel("users").build());
+            links.add(Link.fromUri(CommonConstants.API_URI+"boardgames/").rel("boardgames").build());
+            
+            return links;
+        }
+        
+        /**
+         * Gets the links array.
+         *
+         * @return the links array
+         */
+        public Link[] getLinksArray() {
+            List<Link> links = getLinks();
+            return links.toArray(new Link[links.size()]);
+        }    	
     }
 
 }
